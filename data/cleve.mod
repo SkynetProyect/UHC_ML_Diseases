f28d5a969a42df6b52558d30af3f7d957f43dc55
Samuel Osorio <samuelosur2004@gmail.com>
2026-03-21 17:00:28 -0500

    Creacion del notebook entrega 2, adelanto primeros 2 puntos

---
+++ data/cleve.mod
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a1d2a0369d138b45a761522e897f34dc49d779a1b8f7c323dd9732d632ecc83d
+size 23941
--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-def main():
-    print("Hello from mi-proyecto!")
-
-
-if __name__ == "__main__":
-    main()
+from ucimlrepo import fetch_ucirepo 
+  
+# fetch dataset 
+heart_disease = fetch_ucirepo(id=45) 
+  
+# data (as pandas dataframes) 
+X = heart_disease.data.features 
+y = heart_disease.data.targets 
+  
+# metadata 
+print(heart_disease.metadata) 
+  
+# variable information 
+print(heart_disease.variables) 
